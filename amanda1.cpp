bc11c8a4f05fca9e37c906bfdab61f64f5f36652
Amangopnch <89141049+Amangopnch@users.noreply.github.com>
2021-08-23 20:11:43 +0800

    Add files via upload

---
+++ amanda1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+//(a+b)(a+b) *sqrt(a)
+
+int main(){
+	int a=0;
+	int b=6;
+	
+	printf("too  %d", ((a+b)%2));
+	if(a==b){
+		printf("tenstuu");
+	}
+	for(int i=0;i<6;i++){
+		if(i % 2==0){//tegsh too hed baigaag shal;gah
+		a=a+1;
+		}
+	}
+		printf("%d\n",a);
+}
+	
+ 
+ 
